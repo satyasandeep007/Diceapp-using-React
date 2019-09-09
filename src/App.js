82ce72993eb1744f254528d3f9043d5fcc06daac
lalith <lalithbabumadasi@gmail.com>
2019-09-09 13:53:47 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    number : 1,
+    color: "rgb(0,0,0)"
+  }
+
+  buttonClicked = () => {
+   this.setState({
+     number :  Math.floor((Math.random()*6)+1),
+     color: `rgb(${ Math.floor((Math.random()*256))},${ Math.floor((Math.random()*256))},${ Math.floor((Math.random()*256))})`
+        
+   })
+  }
+  
+  render() {
+    return (
+      <div  style={{backgroundColor: this.state.color}} className="main">
+        <img id="first" src={require(`./diceimages/dice${this.state.number}.png`)} alt={this.state.number} ></img>
+        <button id="button" onClick={this.buttonClicked}>Click here</button>
+      </div>
+    )
+  }
 }
-
-export default App;
